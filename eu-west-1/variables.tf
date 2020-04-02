11eee005505163a52369817bb7f4099014ab2341
Mohan Balasundaram <mohan.balasundaram@primoitsolutions.com>
2020-04-02 16:01:07 +0100

    added description to eu-west-1 variables

--- eu-west-1/variables.tf
+++ eu-west-1/variables.tf
@@ -1,7 +1,19 @@
-variable "region" {}
-variable "project" {}
-variable "environment" {}
-variable "vpc_cidr" {}
-variable "public_cidr_blocks" {}
-variable "availability_zones" {}
+variable "region" {
+  description = "AWS VPC Region"
+}
+variable "project" {
+  description = "Project name"
+}
+variable "environment" {
+  description = "Environment Name"
+}
+variable "vpc_cidr" {
+  description = "AWS VPC CIDR Range"
+}
+variable "public_cidr_blocks" {
+  description = "AWS VPC Subnets CIDR Blocks"
+}
+variable "availability_zones" {
+  description = "AWS VPC Availability zones"
+}
 
